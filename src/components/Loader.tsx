594fe722d2b4243b71b3451cb99409c1ef110fe0
AceViral <sadsadas-07@mail.ru>
2022-04-06 21:04:56 +0300

    debug loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ContentLoader from "react-content-loader";
+import style from "../components/Card/Card.module.scss";
 
 const Loader = () => (
-   <ContentLoader
-      speed={2}
-      width={305}
-      height={482}
-      viewBox="0 0 305 482"
-      backgroundColor="#f3f3f3"
-      foregroundColor="#ecebeb"
-   >
-      <rect x="0" y="0" rx="10" ry="10" width="305" height="482" />
-   </ContentLoader>
+   <div className={style.CardWrapper}>
+      <ContentLoader
+         speed={2}
+         width={305}
+         height={482}
+         viewBox="0 0 305 482"
+         backgroundColor="#f3f3f3"
+         foregroundColor="#ecebeb"
+      >
+         <rect x="0" y="0" rx="10" ry="10" width="305" height="482" />
+      </ContentLoader>
+   </div>
 );
 
 export default Loader;
